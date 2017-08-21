27671ef492170bde8b86f22d9039d2efe8d95504
Aad Schippers <aadschippers@gmail.com>
2017-08-21 12:50:36 +0200

    basedir hidden

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 from mysite.config import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = imported from config.py
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
